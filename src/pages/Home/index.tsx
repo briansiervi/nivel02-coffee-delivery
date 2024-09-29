68322cd5fe9ffc827a6c47bab19e32671653d90b
briansiervi <brianmazini@gmail.com>
2024-09-29 10:46:37 -0300

    feat: tags e responsividade

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,23 +3,45 @@ import bigCoffee from '../../assets/big-coffee.svg'
 export function Home() {
   return (
     <div>
-      <div className='flex flex-row'>
-        <div className='flex flex-col min-w-[36.75rem]'>
-          <span className='text-4xl font-baloo2 text-baloo2-title-xl'>
+      <div className='flex flex-row gap-4 md:gap-8 l:gap-14'>
+        <div className='flex flex-col w-3/5'>
+          <span
+            className='font-baloo2
+            text-baloo2-title-xs
+            s:text-baloo2-title-s
+            md:text-baloo2-title-l
+            l:text-baloo2-title-l
+            xl:text-baloo2-title-xl'
+          >
             Encontre o café perfeito para qualquer hora do dia
           </span>
-          <span>
+          <span className='roboto-text-xs s:roboto-text-s md:roboto-text-m l:roboto-text-l'>
             Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
             hora
           </span>
-          <div>
-            <span>Compra simples e segura</span>
-            <span>Embalagem mantém o café intacto</span>
-            <span>Entrega rápida e rastreada</span>
-            <span>O café chega fresquinho até você</span>
+          <div
+            className='flex flex-row flex-wrap roboto-text-tag
+            mt-4
+            s:mt-8
+            md:mt-8
+            l:mt-16
+            '
+          >
+            <div className='s:basis-1/2 md:basis-1/2 l:basis-1/2'>
+              <span>Compra simples e segura</span>
+            </div>
+            <div className='s:basis-1/2 md:basis-1/2 l:basis-1/2'>
+              <span>Embalagem mantém o café intacto</span>
+            </div>
+            <div className='s:basis-1/2 md:basis-1/2 l:basis-1/2'>
+              <span>Entrega rápida e rastreada</span>
+            </div>
+            <div className='s:basis-1/2 md:basis-1/2 l:basis-1/2'>
+              <span>O café chega fresquinho até você</span>
+            </div>
           </div>
         </div>
-        <div className='min-w-[29.75rem] min-h-[22.5rem]'>
+        <div className='w-2/5'>
           <img
             src={bigCoffee}
             alt='Imagem de um grande copo de café branco, com uma tampa preta e um adesivo também preto, descrito "Coffee delivery", com o logotipo do site'
