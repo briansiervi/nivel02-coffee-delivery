2409b3ac978d9a805e2048e83cb6bb89a46f1f4c
briansiervi <brianmazini@gmail.com>
2024-10-11 08:37:34 -0300

    fix: tamanho máximo da página

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -4,7 +4,7 @@ import { Header } from '../../components/Header'
 
 export function DefaultLayout() {
   return (
-    <LayoutContainer className='px-4'>
+    <LayoutContainer className='px-4 max-w-[1152px]'>
       <Header location='Porto Alegre, RS' />
       <Outlet />
     </LayoutContainer>
