8f66370e7d19626a14e823dc57fe9b891197840c
briansiervi <brianmazini@gmail.com>
2024-10-18 09:08:55 -0300

    feat: atribuindo a cidade no clique do botão

--- src/components/Location/index.tsx
+++ src/components/Location/index.tsx
@@ -3,37 +3,57 @@ import { defaultTheme } from '../../styles/themes/default'
 import { LocationContainer } from './styles'
 import { useEffect, useState } from 'react'
 
+interface locationProps {
+  latitude: number
+  longitude: number
+  language: 'pt'
+  city?: string | null
+}
+
 export function Location() {
-  // const [userLocation, setUserLocation] = useState('')
+  const [city, setCity] = useState<locationProps>()
+  const [coordinates, setCoordinates] = useState<locationProps>()
 
-  useEffect(() => {
-    return getUserLocation()
-  })
-
-  const getUserLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          const { latitude, longitude } = position.coords
-
-          console.log(`Latitude:${latitude}`)
-          console.log(`Longitude:${longitude}`)
-
-          const point = {
-            latitude: latitude,
-            longitude: longitude,
-          }
-          console.log(JSON.stringify(point))
-          // setUserLocation(result.name)
-        },
-        error => {
-          console.error('Geolocation is not supported by this browser.')
-        }
-      )
-    } else {
+  function getUserLocation(): void {
+    if (!navigator.geolocation) {
       console.error('Geolocation is not supported by this browser.')
     }
+
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords
+
+      const point: locationProps = {
+        latitude: latitude,
+        longitude: longitude,
+        language: 'pt',
+        city: null,
+      }
+
+      setCoordinates(point)
+    })
   }
+
+  useEffect(() => {
+    if (coordinates) {
+      fetch(
+        `https://api-bdc.net/data/reverse-geocode-client?${coordinates.latitude}&longitude=${coordinates.longitude}&localityLanguage=${coordinates.language}`
+      ).then(promise => {
+        promise.json().then(res => {
+          console.log(res)
+
+          const location: locationProps = {
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            language: 'pt',
+            city: res.locality,
+          }
+
+          console.log(location)
+          setCity(location)
+        })
+      })
+    }
+  }, [coordinates])
 
   return (
     <LocationContainer onClick={() => getUserLocation()}>
@@ -44,7 +64,7 @@ export function Location() {
         className='bg-purple-light'
       />
 
-      {/* {userLocation && <span className='bg-purple-light'>{userLocation}</span>} */}
+      {city && <span className='bg-purple-light'>{city.city}</span>}
     </LocationContainer>
   )
 }
